b190824404b1cb48f7101b98c0188a8383e3431f
*sKLWdeu74c <s.takahashi@stageinc.jp>
2021-05-11 18:29:37 +0900

    index.htmlの追加

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,2 +1,10 @@
 class CaloriesController < ApplicationController
+
+  def index
+    @japaneses = Japanese.select(:name, :amount, :calories)
+    @westerns = Western.select(:name, :amount, :calories)
+    @chineses = Chinese.select(:name, :amount, :calories)
+    @desserts = Dessert.select(:name, :amount, :calories)
+  end
+
 end
